4dd08b58238c14eae8be633884310628d0b13813
earthmant <earthmant@icloud.com>
2016-06-21 19:34:03 +0300

    CFY-5483 clearning up

--- docker_plugin/tasks.py
+++ docker_plugin/tasks.py
@@ -257,19 +257,20 @@ def pull(client, arguments):
     try:
         for stream in client.pull(**arguments):
             stream_dict = json.loads(stream)
-            ctx.logger.debug('Stream Dict: {0}'.format(str(stream_dict)))
-            if 'Pull complete' in stream_dict:
+            if 'id' in stream_dict:
                 image_id = stream_dict.get('id')
-            if 'Complete' in stream_dict.get('status', ''):
+            elif 'Pull complete' in stream_dict:
+                image_id = stream_dict.get('id')
+            elif 'Complete' in stream_dict.get('status', ''):
                 ctx.logger.info('docker_plugin.tasks.pull: {0}.'.format(
                     stream_dict))
     except APIError as e:
         raise NonRecoverableError(
             'Unabled to pull image: {0}. Error: {1}.'
             .format(arguments, str(e)))
-
-    image_id = utils.get_image_id(
-        arguments.get('tag'), arguments.get('repository'), client)
+    if not image_id:
+        image_id = utils.get_image_id(
+            arguments.get('tag'), arguments.get('repository'), client)
 
     ctx.instance.runtime_properties['image_id'] = image_id
     ctx.logger.info('Pulled image, image_id: {0}'.format(image_id))
